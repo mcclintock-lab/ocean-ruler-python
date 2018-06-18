0fd9d85254c069b5e7ae67f279c85b7e7aa132ea
danyocum <danyocum@gmail.com>
2018-06-18 10:51:52 -0700

    disabled uplaods while testing

--- lambda_function.py
+++ lambda_function.py
@@ -208,11 +208,13 @@ def find_length(is_deployed, req):
         
         #if is_deployed:
         if is_deployed:
-            utils.print_time("calculations done, uploading now", _start_time)
+            utils.print_time("WARNING.........calculations done, WARNING: upload is turned OFF", _start_time)
+            '''
             uploads.upload_worker(rescaled_image, thumb, img_data, name, email, uuid, locCode, picDate, abaloneLength, rating, notes,
                 left_point[0], left_point[1],right_point[0], right_point[1], 
                 left_ruler_point[0], left_ruler_point[1], right_ruler_point[0],right_ruler_point[1], fishery_type, ref_object_size, ref_object_size, ref_object_units, 
                 orig_cols, orig_rows)
+            '''
 
         rval =  {
                     "start_x":str(left_point[0]), "start_y":str(left_point[1]), 
