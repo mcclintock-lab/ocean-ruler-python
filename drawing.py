4074f8d6ab0a3a571b002c294518a0b51105b2ca
danyocum <danyocum@gmail.com>
2019-03-05 15:41:51 -0800

    tweaks to square

--- drawing.py
+++ drawing.py
@@ -358,9 +358,9 @@ def draw_square_contour(base_img, contour, pixelsPerMetric, draw_text, flipDrawi
 
 
     startLinePoint = midpoint(tl, bl)
-    startLinePoint = (int(startLinePoint[0]), int(startLinePoint[1]))
+    startLinePoint = (int(startLinePoint[0])+2, int(startLinePoint[1]))
     endLinePoint = midpoint(tr, br)
-    endLinePoint = (int(endLinePoint[0]), int(endLinePoint[1]))
+    endLinePoint = (int(endLinePoint[0])-2, int(endLinePoint[1]))
     cornerPoints = get_square_corners(base_img, contour)
 
     dBX = abs(startLinePoint[0] - endLinePoint[0])
